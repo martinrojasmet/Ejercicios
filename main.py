01b55d20b3ee41fdde229566b0d742114ed6feda
martinrojasmet <martin.rojas@correo.unimet.edu.ve>
2022-01-17 16:25:37 -0400

    c1e1

---
+++ main.py
@@ -0,0 +1,4 @@
+tu_nombre_aqui = "Martin"
+tu_edad_aqui = "20"
+tu_carrera_aqui = "Ingenieria de sistemas"
+print (f"Hola mundo! mi nombre es {tu_nombre_aqui} y tengo {tu_edad_aqui} años, estudio {tu_carrera_aqui} en la UNIMET!")
